8f21a5bf7583fe5bc169160081106977bfcc03d0
OrtemPressF <vaskoartem.08@gmail.com>
2022-08-01 21:02:51 +0200

    BackEnd(localhost3000ready)

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
+import{HttpClient, HttpHeaders} from '@angular/common/http'
 import { TASKS } from 'src/app/alltasks';
 import { Task } from 'src/app/Task';
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
+    // const tasks = of(TASKS);
+    // return tasks;
   }
 }
